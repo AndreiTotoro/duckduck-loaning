2a3cb66444f01a2b36b50ca6ce4561a134c0b864
AndreiTotoro <andreitotoro@gmail.com>
2023-05-27 20:50:12 +0300

    Implement ability to get all transactions

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ export const revalidate = 0;
 export default async function Home() {
   const owedMoney = await getOwedMoney();
   const interestRate = await getInterestRate();
+  const transactions = await prisma.transactions.findMany({});
 
   return (
     <div className="text-red-500">
@@ -19,6 +20,20 @@ export default async function Home() {
         Current daily interest rate: <b>{interestRate}%</b>
       </h1>
       <TestButton />
+      <div>
+        {transactions.map((transaction) => {
+          return (
+            <div
+              className="flex gap-2"
+              key={transaction.id}>
+              <h1>{transaction.type}</h1>
+              <h1>{transaction.value}</h1>
+              <h1>{transaction.oldBalance}</h1>
+              <h1>{transaction.newBalance}</h1>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
